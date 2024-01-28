ef30d75ebdd41b24aade8dea8a5bbe6203c1ced5
sean-garwood <seansgarwood@gmaill.com>
2024-01-28 12:18:43 -0500

    Validate #entries

--- main.rb
+++ main.rb
@@ -7,12 +7,10 @@ require_relative 'lib/hash_map'
 require_relative 'lib/names'
 
 foo = HashMap.new
-bar = Names.new(1000, 3)
+bar = Names.new(10, 3)
 bar.names.each { |name| foo.set(name) }
 
 puts "\nnames:\n\n#{bar}"
 puts "\nmap:\n#{foo}"
 
-foo.clear!
-
-puts foo
+puts foo.entries
