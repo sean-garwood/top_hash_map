29c1af942c8b69cd84392af13cd8c678457d15d6
sean-garwood <seansgarwood@gmaill.com>
2024-02-05 13:21:31 -0500

    Fix to_s, def #linked helper

--- lib/linked_list/print.rb
+++ lib/linked_list/print.rb
@@ -2,15 +2,20 @@
 
 # print linked lists
 module PrintList
+  def linked(key, value)
+    "{ #{key}: #{value} } -> "
+  end
+
   def to_s
     return '{ }' if empty? || nil?
 
-    print_linked_node = ->(key, value) { print "{ #{key}: #{value} } -> " }
-    current_node = @head
-    while current_node != @tail
-      print_linked_node.call(current_node.key, current_node.value)
-      current_node = current_node.next_node
+    nodes = []
+    curr = head
+    while curr != tail
+      nodes << linked(curr.key, curr.value) unless curr.nil?
+      curr = curr.next_node
     end
-    "{ #{@tail.key}: #{@tail.value} }"
+    tail.nil? ? nil : nodes << "{ #{tail.key}: #{tail.value} }"
+    nodes.join
   end
 end
