274aacf0bbf96b1966539caa51175a87947fc01d
sean-garwood <seansgarwood@gmaill.com>
2024-02-05 15:37:18 -0500

    Test larger maps

--- main.rb
+++ main.rb
@@ -4,13 +4,10 @@ require_relative 'lib/hash_map'
 require_relative 'lib/linked_list'
 require_relative 'lib/entries'
 
-foo  = HashMap.new
-baz = Entries.new(10).entries
-
-
+foo = HashMap.new
+baz = Entries.new(200).entries
 baz.each { |entry| foo.set(entry[0], entry[1]) }
 foo.set('key i know', -1)
-
 puts foo
 puts foo.remove('key i know')
 puts foo
